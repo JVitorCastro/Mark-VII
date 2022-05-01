27e533bd9755a14466d754263bc56f744444db5d
J.Vitor <srvitorcastro@gmail.com>
2022-04-30 23:35:54 -0300

    Enviando outros arquivos do projeto.

---
+++ src/main/java/va/mark7/Mark7Application.java
@@ -0,0 +1,13 @@
+package va.mark7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mark7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mark7Application.class, args);
+	}
+
+}
